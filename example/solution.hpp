bdc861afbaccf7ac8b5b1a029d4384c53a716c1e
feepwang <me@feep.wang>
2021-04-27 16:19:28 +0800

    create repo: add solution example.

---
+++ example/solution.hpp
@@ -0,0 +1,21 @@
+/*
+ * Question
+ */
+
+#ifndef SOLU_SOL_HPP_
+#define SOLU_SOL_HPP_
+
+#include <algorithm>
+#include <string>
+#include <unordered_map>
+
+class Solution {
+   public:
+    // overloading operator()
+    // int operator()(int i,int j);
+
+    // function()
+    // int add(int i,int j);
+};
+
+#endif
