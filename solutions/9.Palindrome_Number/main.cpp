397483de010305fc8576f2c177d8d839b66b98e4
feepwang <me@feep.wang>
2021-05-07 18:34:36 +0800

    update 9.Palindrome_Number

---
+++ solutions/9.Palindrome_Number/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+#include "solution.hpp"
+
+int main() {
+    using namespace std;
+    Solution s;
+
+    int x = 121;
+    cout << std::to_string(s(x)) << endl;
+
+    // print result of solution.
+    cout << endl;
+}
