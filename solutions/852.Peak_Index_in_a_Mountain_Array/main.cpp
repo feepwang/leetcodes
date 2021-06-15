d77b35fa358ed2cc367d36668e3fcee1275482f4
feep <me@feep.wang>
2021-06-15 21:32:46 +0800

    update 852.Peak_Index_in_a_Mountain_Array

---
+++ solutions/852.Peak_Index_in_a_Mountain_Array/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+#include "solution.hpp"
+
+int main() {
+    using namespace std;
+    Solution s;
+
+    vector<int> nums{0, 1, 0};
+    // print result of solution.
+    cout << s(nums) << endl;
+    cout << endl;
+}
