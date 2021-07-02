4f7a7e2f800e6cf60ffdc89e7b44726b55161ad0
feep <me@feep.wang>
2021-07-02 18:14:36 +0800

    update 1833.Maximum_Ice_Cream_Bar

---
+++ solutions/1833.Maximum_Ice_Cream_Bar/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include "solution.hpp"
+
+int main() {
+    using namespace std;
+    Solution s;
+    vector<int> nums{1, 3, 2, 4, 1};
+
+    // print result of solution.
+    cout << s(nums, 7) << endl;
+    cout << endl;
+}
