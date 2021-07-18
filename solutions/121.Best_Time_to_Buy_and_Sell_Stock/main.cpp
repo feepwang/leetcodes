04cd305f389076d3160c92ae1be9c7dd92fbd26e
feepwang <12470612+feepwang@users.noreply.github.com>
2021-07-18 04:09:10 -0700

    update 121.Best_Time_to_Buy_and_Sell_Stock

---
+++ solutions/121.Best_Time_to_Buy_and_Sell_Stock/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+#include "solution.hpp"
+
+int main() {
+    using namespace std;
+    Solution s;
+
+    vector<int> prices{7, 1, 5, 3, 6, 4};
+
+    // print result of solution.
+    cout << s(prices) << endl;
+    cout << endl;
+}
---
+++ solutions/121.Best_Time_to_Buy_and_Sell_Stock/solution.hpp
@@ -0,0 +1,33 @@
+/*
+ * Question
+ */
+
+#ifndef SOLU_SOL_HPP_
+#define SOLU_SOL_HPP_
+
+// include header
+#include <algorithm>
+using std::max;
+using std::min;
+#include <vector>
+using std::vector;
+
+class Solution {
+   public:
+    // overloading operator()
+    int operator()(vector<int>& prices) { return maxProfit(prices); }
+
+    // function()
+    int maxProfit(vector<int>& prices) {
+        int mn = prices[0];
+        int profit = 0;
+        for (const auto& i : prices) {
+            profit = max(profit, i - mn);
+            mn = min(mn, i);
+        }
+
+        return profit;
+    }
+};
+
+#endif
