95a429648f1a91eeda91f2470cc8c1c35a8d0c66
feepwang <12470612+feepwang@users.noreply.github.com>
2021-10-21 15:33:02 +0800

    little improvement. reduce temporary object init.

--- solutions/213.House_Robber_Ⅱ/main.cpp
+++ solutions/213.House_Robber_Ⅱ/main.cpp
@@ -5,7 +5,7 @@
 int main() {
     using namespace std;
     Solution s;
-    vector<int> nums{2, 3, 2};
+    vector<int> nums{200, 3, 140, 20, 10};
 
     // print result of solution.
     cout << s(nums) << endl;
--- solutions/213.House_Robber_Ⅱ/solution.hpp
+++ solutions/213.House_Robber_Ⅱ/solution.hpp
@@ -17,24 +17,47 @@ class Solution {
     int operator()(vector<int>& nums) { return rob(nums); }
 
     // function()
-    int count(const vector<int>& nums) {
-        auto sz = nums.size();
-        if (sz == 1) return nums[0];
+    // int count(const vector<int>& nums) {
+    //     auto sz = nums.size();
+    //     if (sz == 1) return nums[0];
 
-        auto val0 = nums[0], val1 = max(val0, nums[1]), tmp = 0;
-        for (int i = 2; i < sz; i++) {
-            tmp = val1;
-            val1 = max(val0 + nums[i], val1);
-            val0 = tmp;
-        }
-        return val1;
-    }
+    //     auto val0 = nums[0], val1 = max(val0, nums[1]), tmp = 0;
+    //     for (int i = 2; i < sz; i++) {
+    //         tmp = val1;
+    //         val1 = max(val0 + nums[i], val1);
+    //         val0 = tmp;
+    //     }
+    //     return val1;
+    // }
+
+    // int rob(vector<int>& nums) {
+    //     auto sz = nums.size();
+    //     if (sz == 1) return nums[0];
+    //     return max(count(vector<int>(nums.begin() + 1, nums.end())),
+    //                count(vector<int>(nums.begin(), nums.end() - 1)));
+    // }
 
     int rob(vector<int>& nums) {
         auto sz = nums.size();
         if (sz == 1) return nums[0];
-        return max(count(vector<int>(nums.begin() + 1, nums.end())),
-                   count(vector<int>(nums.begin(), nums.end() - 1)));
+
+        auto count = [](vector<int>::const_iterator beg,
+                        vector<int>::const_iterator end) -> int {
+            if (end == beg) return 0;
+            auto sz = end - beg;
+            if (sz == 1) return *beg;
+
+            auto first = *beg, second = max(first, *++beg);
+            for (int tmp{}; ++beg != end;) {
+                tmp = second;
+                second = max(first + *beg, second);
+                first = tmp;
+            }
+            return second;
+        };
+
+        return max(count(nums.cbegin(), nums.cend() - 1),
+                   count(nums.cbegin() + 1, nums.cend()));
     }
 };
 
