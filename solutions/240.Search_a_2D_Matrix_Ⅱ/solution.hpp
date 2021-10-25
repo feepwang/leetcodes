9732478857e74382c4458f22fd0baf23abf253ac
feepwang <12470612+feepwang@users.noreply.github.com>
2021-10-25 20:12:52 +0800

    update 240.Search_a_2D_Matrix_II

---
+++ solutions/240.Search_a_2D_Matrix_Ⅱ/solution.hpp
@@ -0,0 +1,45 @@
+/*
+ * Question
+ *
+ * Write an efficient algorithm that searches for a target value in an m x n
+ * integer matrix. The matrix has the following properties:
+ *
+ * Integers in each row are sorted in ascending from left to right.
+ * Integers in each column are sorted in ascending from top to bottom.
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/search-a-2d-matrix-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+#ifndef SOLU_SOL_HPP_
+#define SOLU_SOL_HPP_
+
+// include header
+#include <vector>
+
+class Solution {
+   public:
+    // overloading operator()
+    bool operator()(std::vector<std::vector<int>>& matrix, const int& target) {
+        return searchMatrix(matrix, target);
+    }
+
+    // function()
+    bool searchMatrix(std::vector<std::vector<int>>& matrix, int target) {
+        if (matrix.empty()) return false;
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int i = 0, j = n - 1;
+        while (i < m && j >= 0) {
+            if (matrix[i][j] == target) return true;
+            if (matrix[i][j] > target)
+                --j;
+            else
+                ++i;
+        }
+        return false;
+    }
+};
+
+#endif
