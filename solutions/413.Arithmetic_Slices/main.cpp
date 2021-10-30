d1222f0df64080f10b73f744c27b4d894589d923
feepwang <12470612+feepwang@users.noreply.github.com>
2021-10-30 01:03:40 -0800

    update 413.Arithmetic_Slices

---
+++ solutions/413.Arithmetic_Slices/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include "solution.hpp"
+
+int main() {
+    using namespace std;
+    Solution s;
+    vector<int> nums{1, 2, 3, 4};
+
+    // print result of solution.
+    cout << s(nums);
+    cout << endl;
+}
