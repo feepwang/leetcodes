485767ef51c9febd97885b657ec4eb1665b55775
feepwang <12470612+feepwang@users.noreply.github.com>
2021-11-19 20:19:58 +0800

    add 397.Integer_Replacement

---
+++ codes/397.Integer_Replacement.hpp
@@ -0,0 +1,54 @@
+﻿/*
+ * Question:
+ * Given a positive integer n, you can apply one of the following operations:
+ *
+ * If n is even, replace n with n / 2.
+ * If n is odd, replace n with either n + 1 or n - 1.
+ * Return the minimum number of operations needed for n to become 1.
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/integer-replacement
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+#ifndef LC_SOLUTION_HPP_
+#define LC_SOLUTION_HPP_
+
+#include <algorithm>
+#include <cmath>
+
+namespace lc {
+
+class Solution {
+   public:
+    int operator()(const int& n) { return integerReplacement(n); }
+
+   private:
+    int integerReplacement(int n) {
+        int result{};
+
+        while (n != 1) {
+            if (n % 2 == 0) {
+                ++result;
+                n /= 2;
+            } else if (n % 4 == 1) {
+                result += 2;
+                n /= 2;
+            } else {
+                if (n == 3) {
+                    result += 2;
+                    n = 1;
+                } else {
+                    result += 2;
+                    n = n / 2 + 1;
+                }
+            }
+        }
+
+        return result;
+    }
+};
+
+}  // namespace lc
+
+#endif
--- main.cc
+++ main.cc
@@ -1,7 +1,7 @@
 #include <iostream>
 
 //  #include "${SOLUTION_HPP}"
-#include "563.Binary_Tree_Tilt.hpp"
+#include "397.Integer_Replacement.hpp"
 
 int main() {
     using std::cout;
@@ -9,14 +9,8 @@ int main() {
 
     lc::Solution s;
 
-    //std::vector<std::vector<int>> t{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-    //lc::TreeNode* root =
-    //    new lc::TreeNode(1, new lc::TreeNode(2), new lc::TreeNode(3));
-
-    lc::TreeNode left(2), right(3), root(1, &left, &right);
-
     //  output
-    cout << s(&root);
+    cout << s(8);
 
     cout << endl;
 }
