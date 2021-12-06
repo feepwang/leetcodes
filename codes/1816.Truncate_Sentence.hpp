185e793b3424732eaa5d4ace9517877f6d810e20
feepwang <12470612+feepwang@users.noreply.github.com>
2021-12-06 19:29:40 +0800

    add 1816.Truncate_Sentence

---
+++ codes/1816.Truncate_Sentence.hpp
@@ -0,0 +1,43 @@
+﻿/*
+ * Question:
+ * A sentence is a list of words that are separated by a single space with no
+ * leading or trailing spaces. Each of the words consists of only uppercase and
+ * lowercase English letters (no punctuation).
+ *
+ * For example, "Hello World", "HELLO", and "hello world hello world" are all
+ * sentences. You are given a sentence s​​​​​​ and an integer
+ * k​​​​​​. You want to truncate s​​​​​​ such that it
+ * contains only the first k​​​​​​ words. Return s​​​​​​
+ * after truncating it.
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/truncate-sentence
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+#ifndef LC_SOLUTION_HPP_
+#define LC_SOLUTION_HPP_
+
+#include <string>
+
+namespace lc {
+
+class Solution {
+   public:
+    std::string operator()(const std::string& s, int k) {
+        return truncateSentence(s, k);
+    }
+
+   private:
+    std::string truncateSentence(std::string s, int k) {
+        int cur = 0;
+        while (k && cur < s.size()) {
+            if (s[++cur] == ' ') k--;
+        }
+        return s.substr(0, cur);
+    }
+};
+
+}  // namespace lc
+
+#endif
