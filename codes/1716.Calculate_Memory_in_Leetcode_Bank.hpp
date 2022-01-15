d91a89f52928ea88d7980361e06894824ff26b6a
feepwang <12470612+feepwang@users.noreply.github.com>
2022-01-15 23:43:53 +0800

    update 1716.Calculate_Memoru_in_Leetcode_Bank

---
+++ codes/1716.Calculate_Memory_in_Leetcode_Bank.hpp
@@ -0,0 +1,29 @@
+﻿/*
+ * Question:
+Hercy wants to save money for his first car. He puts money in the Leetcode bank
+every day.
+
+He starts by putting in $1 on Monday, the first day. Every day from Tuesday to
+Sunday, he will put in $1 more than the day before. On every subsequent Monday,
+he will put in $1 more than the previous Monday. Given n, return the total
+amount of money he will have in the Leetcode bank at the end of the nth day.
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/calculate-money-in-leetcode-bank
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+namespace lc {
+
+class Solution {
+   public:
+    int operator()(int n) { return totalMoney(n); }
+
+   private:
+    int totalMoney(int n) {
+        int a = n / 7, b = n % 7;
+        return (49 + a * 7) * a / 2 + (a * 2 + b + 1) * b / 2;
+    }
+};
+
+}  // namespace lc
--- main.cc
+++ main.cc
@@ -1,21 +1,16 @@
 #include <iostream>
 
 //  #include "${SOLUTION_HPP}"
-#include "2022.Convert_1D_Array_Into_2D_Array.hpp"
+#include "1716.Calculate_Memory_in_Leetcode_Bank.hpp"
 
 int main() {
     using std::cout;
     using std::endl;
 
     lc::Solution s;
-    std::vector<int> nums{1, 2, 3, 4};
+    
 
     //  output
-    for (const auto& vec : s(nums, 2, 2)) {
-        for (auto i : vec) {
-            cout << i << " ";
-        }
-        cout << endl;
-    }
+    cout << s(10);
     cout << endl;
 }
