f33b96f8eaeaa988eab004d0ae4c63a89f7dd94a
feepwang <12470612+feepwang@users.noreply.github.com>
2022-01-28 23:42:32 +0800

    add 1996.The_Number_of_Weak_Characters_in_the_Game

--- main.cc
+++ main.cc
@@ -1,11 +1,15 @@
 #include <iostream>
+#include <vector>
 
-#include "2047.Number_of_Valid_Words_in_a_Sentence.hpp"
+#include "1996.The_Number_of_Weak_Characters_in_the_Game.hpp"
 
 int main() {
     using namespace std;
 
     lc::Solution sol{};
 
-    cout << sol("cat and dog") << endl;
+    std::vector<std::vector<int>> properties{{5, 5}, {6, 3}, {3, 6}};
+
+    cout << sol(properties);
+    cout << endl;
 }
