9d6505d32f4b1b18c6edf38ecc95876a8712c741
feepwang <12470612+feepwang@users.noreply.github.com>
2022-02-03 23:37:37 +0800

    update 1765.Map_of_Highest_Peak

--- main.cc
+++ main.cc
@@ -1,13 +1,20 @@
 #include <iostream>
 #include <vector>
 
-#include "1414.Find_the_Minimum_Number_of_Fibonacci_Numbers_Whose_Sum_Is_K.hpp"
+#include "1765.Map_of_Highest_Peak.hpp"
 
 int main() {
     using namespace std;
 
     lc::Solution sol{};
 
-    cout << sol(7);
+    std::vector<std::vector<int>> isWater{{0, 1}, {0, 0}};
+
+    for (const auto& nums : sol(isWater)) {
+        for (auto i : nums) {
+            cout << i << ' ';
+        }
+        cout << endl;
+    }
     cout << endl;
 }
