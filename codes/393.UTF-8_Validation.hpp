ecd53d8937ccd44e5ae4343371432458b423bb34
feepwang <12470612+feepwang@users.noreply.github.com>
2022-03-15 21:40:10 +0800

    add 393.UTF-8_Validation

---
+++ codes/393.UTF-8_Validation.hpp
@@ -0,0 +1,63 @@
+/*
+ * Question:
+给定一个表示数据的整数数组 data ，返回它是否为有效的 UTF-8 编码。
+
+UTF-8 中的一个字符可能的长度为 1 到 4 字节，遵循以下的规则：
+
+    对于 1 字节 的字符，字节的第一位设为 0 ，后面 7 位为这个符号的 unicode 码。
+    对于 n 字节 的字符 (n > 1)，第一个字节的前 n 位都设为1，第 n+1 位设为 0
+，后面字节的前两位一律设为 10 。剩下的没有提及的二进制位，全部为这个符号的
+unicode 码。
+
+这是 UTF-8 编码的工作方式：
+
+   Char. number range  |        UTF-8 octet sequence
+      (hexadecimal)    |              (binary)
+   --------------------+---------------------------------------------
+   0000 0000-0000 007F | 0xxxxxxx
+   0000 0080-0000 07FF | 110xxxxx 10xxxxxx
+   0000 0800-0000 FFFF | 1110xxxx 10xxxxxx 10xxxxxx
+   0001 0000-0010 FFFF | 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
+
+注意：输入是整数数组。只有每个整数的 最低 8 个有效位
+用来存储数据。这意味着每个整数只表示 1 字节的数据。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/utf-8-validation
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+#include <vector>
+
+namespace lc {
+
+class Solution {
+   public:
+    bool operator()(std::vector<int>& data) { return validUtf8(data); }
+
+   private:
+    bool validUtf8(std::vector<int>& data) {
+        int count = 1;
+        for (auto i : data) {
+            if (count == 1) {
+                if (i >> 7 == 0) {
+                    continue;
+                } else if (i >> 5 == 0x6) {
+                    count = 2;
+                } else if (i >> 4 == 0xe) {
+                    count = 3;
+                } else if (i >> 3 == 0x1e) {
+                    count = 4;
+                } else
+                    return false;
+            } else {
+                count--;
+                if (i >> 6 ^ 0x2) return false;
+            }
+        }
+        if (count ^ 1) return false;
+        return true;
+    }
+};
+
+}  // namespace lc
--- main.cc
+++ main.cc
@@ -1,12 +1,16 @@
 #include <iostream>
 
-#include "258.Add_Digits.hpp"
+#include "393.UTF-8_Validation.hpp"
 
 int main() {
     using namespace std;
 
     lc::Solution sol{};
+    std::vector<int> data{197, 130, 1};
 
-    cout << sol(38);
+    if (sol(data)) {
+        cout << "true" << endl;
+    } else
+        cout << "false" << endl;
     cout << endl;
 }
