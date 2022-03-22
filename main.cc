67d5119d32adf1f02674aea1ac9fd053189d6451
feepwang <12470612+feepwang@users.noreply.github.com>
2022-03-22 22:23:14 +0800

    add 2038.

--- main.cc
+++ main.cc
@@ -1,13 +1,15 @@
 #include <iostream>
 
-#include "720.Longest_Word_in_Dictionary.hpp"
+#include "2038.Remove_Colored_Pieces_if_Both_Neighbors_are_the_Same_Color.hpp"
 
 int main() {
     using namespace std;
 
     lc::Solution sol{};
-    vector<string> words{"w", "wo", "wor", "worl", "world"};
 
-    cout << sol(words) << endl;
+    if (sol("AAABABB"))
+        cout << "Alice wins" << endl;
+    else
+        cout << "Bob wins" << endl;
     cout << endl;
 }
